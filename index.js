5f951086324e9286a4b4929a048fe8e54110a211
DolzhenkovAP <ra3wok@gmail.com>
2023-04-07 16:15:53 +0300

    2

--- index.js
+++ index.js
@@ -1,21 +1,126 @@
+const getDataFromServer= async function (url) {
+  let response = await fetch(url);
+  if (response.ok) {
+    return await response.json();
+  }
+  else {
+    console.log("Ошибка HTTP: " + response.status);
+  }
+}
+
+
+
 
 let linkmarks = document.getElementById("linkmarks");
 
-
-linkmarksBlocks = Object.values(linkmarks.children);
-
-let elCount=1;
-linkmarksBlocks.forEach(element => {
-  element.id = "block_" +elCount;
-  elCount++;
-})
+linkmarks.classList.add("linkmarks");
 
 
-linkmarks.onmouseover = (pointingObj) => {
+let linkmarksJSON={};
 
-  console.log(pointingObj.target.getBoundingClientRect());
+
+
+const mainFunc = () => {
+
+
+  document.onmousedown = (pointingObj) => {
+    let coords = pointingObj.target.getBoundingClientRect();
+    if(pointingObj.target.className==='linkmarks__block') {
+
+
+
+      pointingObj.target.style.border = "1px dotted red";
+      pointingObj.target.style.zIndex = "-123123";
+
+    }
+  }
+
+  document.onmouseup = (pointingObj) => {pointingObj.target.style='';}
+  document.onmouseout = (pointingObj) => {pointingObj.target.style='';}
+
+  let addNewBlockElement = document.getElementById('addNewBlock');
+  addNewBlockElement.onmousedown = () => {
+      let newBlock = document.createElement('div');
+      newBlock.classList.add("linkmarks__block");
+      newBlock.innerHTML='<div class="linkmarks__block-header">' +
+        '<h2 class="linkmarks__block-title" ><input type="text" /></h2>' +
+        '<div class="linkmarks__block-close">Х</div>' +
+        '</div>';
+      newBlock.getElementsByClassName('linkmarks__block-close')[0]
+        .onmousedown = (event)=> {
+        event.target.parentElement.parentElement.remove();
+      }
+      linkmarks.insertBefore( newBlock,addNewBlockElement);
+    }
+
+
+
+  Object.values(linkmarks.getElementsByClassName('linkmarks__block-close'))
+  .forEach(
+    element =>{ element.onmousedown = (event)=> {
+      event.target.parentElement.parentElement.remove();
+    }}
+  )
+
+
 
 }
+
+
+
+getDataFromServer("links.json").then((result)=>{
+
+  linkmarksJSON = result;
+  localStorage.linkmarks = JSON.stringify(result);
+  let key=undefined;
+  for (key in linkmarksJSON) {
+
+    let blockLinksString ='<ul class="linkmarks__block-links" >';
+    linkmarksJSON[key].forEach(el => {
+
+      let url = new URL(el.url);
+
+      blockLinksString = blockLinksString + '<li>' +
+        '<a href="' + el.url + '" target="_blank"><img src="'+url.protocol+'//'+url.host+'/favicon.ico" alt="">'+el.title+'</a>' +
+        '</li>';
+      });
+
+    blockLinksString = blockLinksString + '</ul>';
+
+    let block = document.createElement("div");
+    block.classList.add("linkmarks__block");
+    block.innerHTML = '<div class="linkmarks__block-grab">______</div><div class="linkmarks__block-header">' +
+      '<h2 class="linkmarks__block-title" >' + key + '</h2> <div class="linkmarks__block-close">Х</div>' +
+      '</div>' + blockLinksString;
+
+
+    linkmarks.appendChild(block);
+  }
+
+  let addBlock = document.createElement("div");
+  addBlock.classList.add("linkmarks__addNew");
+  addBlock.innerHTML='+ Добавить блок';
+  addBlock.id='addNewBlock';
+  linkmarks.appendChild(addBlock);
+  return linkmarks;
+}).then(mainFunc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -26,8 +131,8 @@ linkmarks.onmouseover = (pointingObj) => {
 
 //linkmarks.onmouseover = linkmarks.onmouseout = handler;
 
-
 /*
+
 const choosingElement = (el) => {
   if(el.target.id !== 'list'){
     console.log(el.target.innerText);
